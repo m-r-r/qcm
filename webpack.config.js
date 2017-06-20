f105f96883c2cb24cee118272ef7e9a3536102f0
m-r-r <raybaudroigm@gmail.com>
2017-06-20 21:06:43 +0200

    Switch to Webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -8,17 +8,14 @@ const ROOT_DIR = __dirname;
 const SRC_DIR = path.join(ROOT_DIR, 'src');
 const NODE_MODULES_DIR = path.join(ROOT_DIR, 'node_modules');
 const BUILD_DIR = path.join(ROOT_DIR, 'build');
-const HOST = process.env.IP || 'localhost';
-const PORT = process.env.PORT || 8080;
-
 
 const config = {
+  target: 'web',
   context: SRC_DIR,
   entry: {
     client: ["./client/style.scss", "./client/main.js"],
-    example: ["file?name=[name].[ext]?v=[hash]!./example.json"],
+    example: ["file-loader?name=[name].[ext]?v=[hash]!./example.json"],
   },
-  target: 'web',
 
   output: {
     path: BUILD_DIR,
@@ -27,61 +24,55 @@ const config = {
   },
 
   resolve: {
-    root: path.resolve(SRC_DIR),
-    extensions: ['', '.js', '.jsx', '.json', '.scss'],
-    aliases: {}
+    extensions: ['.js', '.jsx', '.json', '.scss'],
   },
 
   module: {
-    preLoaders: [
+    rules: [
       {
         test: /\.jsx?$/,
-        loader: 'eslint',
         include: [SRC_DIR],
-      }
-    ],
-    loaders: [
-      {
-        loader: 'babel',
-        test: /\.jsx?$/,
-        include: [SRC_DIR],
+        use: 'babel-loader',
       },
       {
-        loader: 'json',
         test: /\.json?$/,
         include: [SRC_DIR, NODE_MODULES_DIR],
-        exclude: [path.join(SRC_DIR, 'example.json')]
+        exclude: [path.join(SRC_DIR, 'example.json')],
+        use: 'json-loader'
       },
       {
-        loader: ExtractTextPlugin.extract('style', 'css?sourceMap!sass'),
         test: /\.scss$/,
         include: [SRC_DIR, NODE_MODULES_DIR],
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: "css-loader" }, 
+            {
+              loader: "sass-loader", 
+              options: {
+                sourceMap: true,
+                includePaths: [path.join(NODE_MODULES_DIR, 'normalize.css')],
+              }
+            }
+          ]
+        })
       },
 
-      { test: /\.woff(\?.*)?$/,  loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff' },
-      { test: /\.woff2(\?.*)?$/, loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff2' },
-      { test: /\.otf(\?.*)?$/,   loader: 'file?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=font/opentype' },
-      { test: /\.ttf(\?.*)?$/,   loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/octet-stream' },
-      { test: /\.eot(\?.*)?$/,   loader: 'file?prefix=fonts/&name=[path][name].[ext]' },
-      { test: /\.svg(\?.*)?$/,   loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=image/svg+xml' },
-      { test: /\.(png|jpg)$/,    loader: 'url?limit=8192' }
+      { test: /\.woff(\?.*)?$/,  use: 'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff' },
+      { test: /\.woff2(\?.*)?$/, use: 'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff2' },
+      { test: /\.otf(\?.*)?$/,   use: 'file-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=font/opentype' },
+      { test: /\.ttf(\?.*)?$/,   use: 'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/octet-stream' },
+      { test: /\.eot(\?.*)?$/,   use: 'file-loader?prefix=fonts/&name=[path][name].[ext]' },
+      { test: /\.svg(\?.*)?$/,   use: 'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=image/svg+xml' },
+      { test: /\.(png|jpg)$/,    use: 'url-loader?limit=8192' }
     ],
+  },
 
-    sassLoader: {
-      sourceMap: true,
-      includePaths: [path.join(NODE_MODULES_DIR, 'normalize.css')],
-    },
-
-    devServer: {
-      contentBase: BUILD_DIR,
-      hot: true,
-      inline: true,
-      progress: true,
-      stats: 'errors-only',
-      watch: true,
-      host: HOST,
-      port: PORT,
-    },
+  devServer: {
+    contentBase: BUILD_DIR,
+    hot: true,
+    inline: true,
+    quiet: true
   },
   devtool: '#eval-source-map',
 
@@ -103,9 +94,8 @@ const config = {
 if (DEV) {
   config.plugins.push(
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin()
   );
-  //config.entry.client.unshift("webpack/hot/dev-server");
 } else {
   config.plugins.push(
     new webpack.optimize.OccurrenceOrderPlugin(),
@@ -119,13 +109,6 @@ if (DEV) {
     })
   );
 }
-
-config.eslint = {
-  configFile: path.join(ROOT_DIR, '.eslintrc'),
-  emitWarning: DEV,
-};
-
-
 
 
 module.exports = config;
